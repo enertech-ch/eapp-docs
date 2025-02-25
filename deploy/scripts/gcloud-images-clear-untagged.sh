8aac40ec55963a1feed209486200edeba383e858
Dominik Kohler <dominik.kohler@komail.ch>
2025-02-25 18:09:57 +0100

    Migrate to github action

--- deploy/scripts/gcloud-images-clear-untagged.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-IMAGE_NAME=$1
-
-gcloud container images list-tags $IMAGE_NAME --filter='-tags:*' --format='get(digest)' --limit=unlimited \
-       | awk -v IMAGE_NAME="$IMAGE_NAME" '{print IMAGE_NAME "@" $1}' \
-       | xargs --no-run-if-empty gcloud container images delete --quiet
